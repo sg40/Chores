b28d3df53caf46cfc656c8c452940359c593ea59
sweatygoat <sweatygoat@pop-os.localdomain>
2025-04-29 09:51:55 -0400

    updated user model

--- chores/admin.py
+++ chores/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import ChoreGroup, Chore
+from .models import ChoreGroup, Chore, Household, User
 
 # Register your models here.
 admin.site.register(Chore)
 admin.site.register(ChoreGroup)
-
+admin.site.register(Household)
+admin.site.register(User)
 
