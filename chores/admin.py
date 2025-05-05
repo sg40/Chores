4a131c600ab80ebc165df35c3ace4387a853d7f9
sweatygoat <sweatygoat@pop-os.localdomain>
2025-05-05 13:48:09 -0400

    added chore data to group a

--- chores/admin.py
+++ chores/admin.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 from .models import ChoreGroup, Chore, Person, Days
 
+
+class ChoreAdmin(admin.ModelAdmin):
+    list_display = ('name', 'chore_group')
+
 # Register your models here.
-admin.site.register(Chore)
+admin.site.register(Chore, ChoreAdmin)
 admin.site.register(ChoreGroup)
 admin.site.register(Person)
 admin.site.register(Days)
-
