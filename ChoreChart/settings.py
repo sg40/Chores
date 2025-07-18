46f401a36ae16830e3205b04ceaa9cac7c0bbbf5
sweatygoat <sweatygoat@pop-os.localdomain>
2025-07-18 12:22:26 -0400

    collected static

--- ChoreChart/settings.py
+++ ChoreChart/settings.py
@@ -112,7 +112,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
-STATIC_ROOT = '/home/ubuntu/Chores/static'
+STATIC_ROOT = BASE_DIR / 'static'
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
