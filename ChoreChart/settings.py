b20bd6927d0575572ed8f91baa3113611dcdef12
sweatygoat <sweatygoat@pop-os.localdomain>
2025-07-24 14:17:30 -0400

    Switch to PostgreSQL database

--- ChoreChart/settings.py
+++ ChoreChart/settings.py
@@ -76,11 +76,14 @@ WSGI_APPLICATION = 'ChoreChart.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'chores_db',
+        'USER': 'chores_user',
+        'PASSWORD': '4RtBNEeA',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
